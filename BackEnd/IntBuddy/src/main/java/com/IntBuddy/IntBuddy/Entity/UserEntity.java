7a82255a0b6a3cdc0fe16de7642bc4deb43edf26
N-1218 <malusarenamrata88@gmail.com>
2026-03-24 22:12:22 +0530

    backend

---
+++ BackEnd/IntBuddy/src/main/java/com/IntBuddy/IntBuddy/Entity/UserEntity.java
@@ -0,0 +1,5 @@
+package com.IntBuddy.IntBuddy.Entity;
+
+public class UserEntity {
+
+}
---
+++ BackEnd/IntBuddy/src/test/java/com/IntBuddy/IntBuddy/IntBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntBuddy.IntBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
